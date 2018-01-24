b1613982fc3ddff3b6182f78c8e4a815e1a064ad
Mihail <miket@newmail.ru>
2018-01-24 20:58:26 +0300

    Add style for 'td'

--- php/lesson1_1/about.php
+++ php/lesson1_1/about.php
@@ -1,28 +1,50 @@
 <?php
 
 $name = 'Михаил';
-$age = '36';
+$age = '37';
 $email = 'mihail.treschalin@ya.ru';
 $aboutMe = 'Изучаю Web программирование. Хочу сменить профессию';
-$city = 'Электроугли';
+$city = 'Балашиха';
 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-	<meta charset="utf-8">
-</head>
-<body>
-<h2>Домашняя работа №1</h2>
-<table>
-	<tr><td>Имя</td> <td><?= $name ?></td></tr>
-	<tr><td>Возраст</td> <td><?= $age ?></td></tr>
-	<tr><td>Адрес электронной почты</td> <td><a href="mailto:<?= $email ?>"><?= $email ?></a></td></tr>
-	<tr><td>Город</td> <td><?= $city ?></td></tr>
-	<tr><td>О себе</td> <td><?= $aboutMe ?></td></tr>
-</table>
-
-</body>
+	<head>
+		<title>Домашняя работа №1</title>
+		<meta charset="utf-8">
+		<style>
+			td {
+				padding: 0 5px 0 5px;
+				font-size: 18px;
+			}
+		</style>
+	</head>
+	<body>
+		<h2>Страница пользователя Михаил</h2>
+		<table>
+			<tr>
+				<td>Имя</td>
+				<td><?= $name ?></td>
+			</tr>
+			<tr>
+				<td>Возраст</td>
+				<td><?= $age ?></td>
+			</tr>
+			<tr>
+				<td>Адрес электронной почты</td>
+				<td>
+					<a href="mailto:<?= $email ?>"><?= $email ?></a>
+				</td>
+			</tr>
+			<tr>
+				<td>Город</td>
+				<td><?= $city ?></td>
+			</tr>
+			<tr>
+				<td>О себе</td>
+				<td><?= $aboutMe ?></td>
+			</tr>
+		</table>
+	</body>
 </html>
