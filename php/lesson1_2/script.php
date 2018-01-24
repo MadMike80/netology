5229f86e230b3c50831579c591d61126169c0f7a
Mihail <miket@newmail.ru>
2018-01-25 01:13:31 +0300

    Add lesson1_2

---
+++ php/lesson1_2/script.php
@@ -0,0 +1,28 @@
+<?php
+
+    $userNumber = rand(0,100); // $userNumber = $_GET['x'];
+    $number1 = 1;
+    $number2 = 1;
+
+    echo "Числовой ряд: $number1 ";
+
+    while ($number1 != $userNumber) {
+
+        if ($number1 > $userNumber) {
+            echo "<br/>";
+            echo "Задуманное число: $userNumber не входит в числовой ряд";
+            break;
+        }
+
+    $number3 = $number1;
+    $number1 = $number1 + $number2;
+    $number2 = $number3;
+    echo "$number1 ";
+    }
+
+    if ($number1 == (int)$userNumber){
+        echo "<br/>";
+        echo "Задуманное число: $userNumber входит в числовой ряд";
+    }
+
+?>
