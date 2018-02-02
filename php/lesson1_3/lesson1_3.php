b7ec2d7cb61f7b38904e46cdc9be8bed8d2e85e3
Mihail <miket@newmail.ru>
2018-02-02 19:16:20 +0300

    second fix

--- php/lesson1_3/lesson1_3.php
+++ php/lesson1_3/lesson1_3.php
@@ -1,18 +1,17 @@
 <?php
 
-$animals = array(
-    "Eurasian" => array("Ursus arctos", "Meles meles", "Martes zibellina", "Viverra lutra Linnaeus"),
-    "Africa" => array("Lycaon pictus somalicus", "Papio cynocephalus", "Camelus", "Panthera leo"),
-    "Australia" => array("Macropus", "Ornithorhynchus anatinus", "Tachyglossus aculeatus aculeatus", "Sarcophilus laniarius"),
-    "NorthAmerica" => array("Hystrix cristata", "Heloderma suspectum cinctum", "Mephitis", "Canis latrans"),
-    "SouthAmerica" => array("Lama", "Leopardus pardalis", "Puma concolor anthonyi", "Folivora")
-);
+$animals = [
+    "Eurasian" => ["Ursus arctos", "Meles meles", "Martes zibellina", "Viverra lutra Linnaeus"],
+    "Africa" => ["Lycaon pictus somalicus", "Papio cynocephalus", "Camelus", "Panthera leo"],
+    "Australia" => ["Macropus", "Ornithorhynchus anatinus", "Tachyglossus aculeatus aculeatus", "Sarcophilus laniarius"],
+    "NorthAmerica" => ["Hystrix cristata", "Heloderma suspectum cinctum", "Mephitis", "Canis latrans"],
+    "SouthAmerica" => ["Lama", "Leopardus pardalis", "Puma concolor anthonyi", "Folivora"]
+];
 
-    $newAnimals = array();
-    $firstWordInContinent = array();
 foreach($animals as $continent => $animalName) {
     foreach ($animalName as $value) {
-        if (strpos($value, ' ') !== false && substr_count($value, ' ') <= 1) {
+        /*if (strpos($value, ' ') !== false && substr_count($value, ' ') <= 1)*/
+        if (str_word_count($value) === 2) {
             $newAnimals[$continent][] = $value;
             list($first, $second) = explode(' ', $value, 2);
             $firstWord[] = $first;
@@ -25,12 +24,11 @@ foreach($animals as $continent => $animalName) {
 shuffle($firstWord);
 shuffle($secondWord);
 
-    $fantasticAnimals = array();
 for ($i = 0; $i < count($firstWord); $i++) {
     $fantasticAnimals[] = $firstWord[$i] . " " . $secondWord[$i];
     echo "$fantasticAnimals[$i]</br>";
 }
-    $newFantasticAnimals = array();
+
 foreach($firstWordInContinent as $nameContinent => $firstWordItem) {
     foreach ($firstWordItem as $item){
         foreach ($fantasticAnimals as $name) {
@@ -42,12 +40,14 @@ foreach($firstWordInContinent as $nameContinent => $firstWordItem) {
 }
 
 foreach($newFantasticAnimals as $continentName => $nameAnimals) {
+    $fantasticName = implode(', ', $nameAnimals);
     echo '<h2>' . "$continentName" . '</h2>';
-    foreach ($nameAnimals as $key => $value) {
+    echo $fantasticName;
+    /*foreach ($nameAnimals as $key => $value) {
         if ($key===0) {
             echo "$value";
         } else {
             echo ', ' . "$value";
         }
-    }
+    }*/
 }
